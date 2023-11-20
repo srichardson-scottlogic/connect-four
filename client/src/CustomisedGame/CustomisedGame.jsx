e8e161c136466784a36edf17f58540f97623154a
Sophie Richardson (She/Her) <srichardson@scottlogic.com>
2023-11-20 15:09:14 +0000

    Update styling

--- client/src/CustomisedGame/CustomisedGame.jsx
+++ client/src/CustomisedGame/CustomisedGame.jsx
@@ -11,28 +11,42 @@ export default function CustomisedGame() {
     const [playerColour, setPlayerColour] = useState(null);
     const [redIsNext, setRedIsNext] = useState(null);
 
+    const [numberOfColumns, setNumberOfColumns] = useState(7);
+    const [numberOfRows, setNumberOfRows] = useState(6);
+    const [numberToConnect, setNumberToConnect] = useState(4);
+    const [inputtedRoomId, setInputtedRoomId] = useState(null);
+
     const { sendJsonMessage, lastJsonMessage } = useWebSocket(process.env.REACT_APP_WS_URL, {
         share: true,
     });
+
+    const handleBoardCustomisationSubmit = () => {
+        const message = {
+            action: "customise",
+            numberOfColumns: numberOfColumns,
+            numberOfRows: numberOfRows,
+            numberToConnect: numberToConnect
+        }
+
+        if (!gameCustomised) {
+            message.action = "create";
+        }
+
+        sendJsonMessage(message);
+    }
+
+    const handleJoinGame = () => {
+        sendJsonMessage({
+            action: "join",
+            roomId: inputtedRoomId
+        });
+    }
 
     const handlePlay = useCallback((nextSquares, nextWinner, redIsNext) => {
         setSquares(nextSquares);
         setWinner(nextWinner);
         setRedIsNext(redIsNext);
     }, [setSquares, setWinner, setRedIsNext]);
-
-    const handleJoinGame = (e) => {
-        e.preventDefault();
-
-        const form = e.target;
-        const formData = new FormData(form);
-        const formJson = Object.fromEntries(formData.entries());
-
-        sendJsonMessage({
-            action: "join",
-            roomId: formJson.gameRoomId
-        });
-    }
 
     useEffect(() => {
         if (lastJsonMessage) {
@@ -66,28 +80,6 @@ export default function CustomisedGame() {
         }
     }, [lastJsonMessage, handlePlay]);
 
-    const handleBoardCustomisationSubmit = (e) => {
-        e.preventDefault();
-
-        const form = e.target;
-        const formData = new FormData(form);
-        const formJson = Object.fromEntries(formData.entries());
-
-        const message = {
-            action: "customise",
-            numberOfColumns: Number(formJson.numberOfColumns),
-            numberOfRows: Number(formJson.numberOfRows),
-            numberToConnect: Number(formJson.connectNumber)
-        }
-
-        if (!gameCustomised) {
-            message.roomId = formData.roomId;
-            message.action = "create";
-        }
-
-        sendJsonMessage(message);
-    }
-
     let status;
     if (winner) {
         status = winner === "Draw" ? "It's a draw 🥱" : (playerColour === winner) ? "Congratulations, you won 😁" : "You lost, better luck next time 😓";
@@ -117,50 +109,57 @@ export default function CustomisedGame() {
                 {gameCustomised && <div className="playerColour">You are {playerColour}</div>}
                 {(gameCustomised && !winner) && <div className="whoIsNext">{whoIsNext}</div>}
             </div>
-            <form className="customiseGame" onSubmit={handleBoardCustomisationSubmit}>
-                <div className="formContainer">
-                    <label>
-                        Number of Columns:
-                        <input
-                            defaultValue={7}
-                            name="numberOfColumns"
-                        />
-                    </label>
-                    <label>
-                        Number of Rows:
-                        <input
-                            defaultValue={6}
-                            name="numberOfRows"
-                        />
-                    </label>
-                    <label>
-                        Number to Connect:
-                        <input
-                            defaultValue={4}
-                            name="connectNumber"
-                        />
-                    </label>
-                </div>
-                <div className="winnerStatus">{status}</div>
-                {gameCustomised && <div className="resetButtonContainer">
-                    <button className="reset" type="submit">{resetStatus}</button >
-                </div >}
-                {!gameCustomised && <div className="twoPlayerButtonContainer">
-                    <button className="reset" type="submit">Create Online Room</button >
-                </div >}
-                {/* {!gameCustomised && <div className="onePlayerButtonContainer">
-                    <button className="reset" type="submit" onClick={handleBoardCustomisationSubmit}>Pass and Play</button >
-                </div >} TODO: Implement Pass and Play */}
-            </form>
-            {(!gameCustomised && !roomId) && <form className="joinExistingGame" onSubmit={handleJoinGame}>
-                <div className="joinGameContainer">
-                    <label>
-                        Game Room ID:
-                        <input name="gameRoomId" />
-                    </label>
-                    <button className="join" type="submit">Join Existing Game</button >
-                </div >
-            </form>}
+            <div className="customiseGame">
+                <label >
+                    Number of Columns:
+                    <input
+                        name="numberOfColumns"
+                        value={numberOfColumns}
+                        onInput={e => setNumberOfColumns(Number(e.target.value))}
+                    />
+                </label>
+                <label>
+                    Number of Rows:
+                    <input
+                        name="numberOfRows"
+                        value={numberOfRows}
+                        onInput={e => setNumberOfRows(Number(e.target.value))}
+                    />
+                </label>
+                <label>
+                    Number to Connect:
+                    <input
+                        name="connectNumber"
+                        value={numberToConnect}
+                        onInput={e => setNumberToConnect(Number(e.target.value))}
+                    />
+                </label>
+            </div >
+            <div className="winnerStatus">{status}</div>
+            {gameCustomised && <div className="resetButtonContainer">
+                <button className="buttonText" onClick={handleBoardCustomisationSubmit}>{resetStatus}</button >
+            </div >}
+            {(!gameCustomised && !roomId) && <div className="text">Pick (carefully) how you want to play:</div>}
+            {(!gameCustomised && !roomId) && <div className="twoPlayerButtonContainer">
+                <button className="buttonText" onClick={handleBoardCustomisationSubmit}>Create Online Room</button >
+            </div >}
+            {(!gameCustomised && !roomId) && <div className="text">OR</div>}
+            {(!gameCustomised && !roomId) && <div className="joinTwoPlayerGameContainer">
+                <button className="buttonText" onClick={handleJoinGame}>Join Existing Game</button >
+                <label>
+                    <input
+                        className="gameRoomId"
+                        placeholder="put game room code here"
+                        value={inputtedRoomId}
+                        onInput={e => setInputtedRoomId(e.target.value)}
+                    />
+                </label>
+            </div>}
+            {(!gameCustomised && !roomId) && <div className="text">OR</div>}
+            {(!gameCustomised && !roomId) && <div className="onePlayerButtonContainer">
+                <button className="buttonText">Pass and Play (Not Implemented Yet)</button >
+            </div >}
+            {(!gameCustomised && !roomId) && <div className="text">By the way, once you choose there is no return 😇</div>}
             {gameCustomised && < Game squares={squares} winner={winner} roomId={roomId} playerColour={playerColour} redIsNext={redIsNext} />}
         </>
     );
